9682bf56c881bd08668b6249c575bcc4ea3186fc
Ricardo <ricardoaugustogarcia@hotmail.com>
2018-10-18 12:06:45 -0300

    Criação do menu e dos page-headers e utilização do react-router v3

---
+++ src/Todo/Todo.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import PageHeader from "../Template/PageHeader";
+
+export default class Todo extends Component {
+  render() {
+    return <PageHeader name="Tarefas" small="Cadastro" />;
+  }
+}
